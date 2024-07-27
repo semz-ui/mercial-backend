c9c733091a388a589155ef8ea48a57af551b0614
semz-ui <Michaeltimilehin36@gmail.com>
2024-07-27 14:50:20 +0100

    mkd

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 
-//Routes
+//Routesdd
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/message", messageRoutes);
